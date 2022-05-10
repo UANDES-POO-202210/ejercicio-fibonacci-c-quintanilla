d7a1708fe03c2de308bc46bf4ac9fa04c8950a10
c-quintanilla <mcquitanilla@miuandes.cl>
2022-05-09 23:08:05 -0300

    primer commit

---
+++ quintanilla_fib.js
@@ -0,0 +1,25 @@
+var num = require('fs');
+var num_m = num.readFileSync('numero.txt', 'utf8');
+let num_f = num_m.toString();
+
+
+function fibonacci(num){
+var lista=[0,1];
+var a = 0;
+var b = 1;
+var c=0;
+}
+/*
+for (c<num, var res=0,){
+    a+b=res;
+
+    c++;
+
+}
+
+return res;
+}
+
+console.log("El resultado de la serie de fibonaci para el numero", num, "es:");
+console.log(res);
+*/
